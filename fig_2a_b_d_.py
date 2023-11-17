2f1c9dd4c82cf062ea71aa11f792ffa9f143d7bc
Perfus <sufrep@gmail.com>
2023-11-17 16:54:30 +0300

    Update fig_2a_b_d_.py

--- fig_2a_b_d_.py
+++ fig_2a_b_d_.py
@@ -39,6 +39,64 @@ for file in filelist:
 
 data['group'] = data.apply(lambda row: group(row['nLabel']), axis = 1)
 
-selected = ['pX037', 'pNK497']
+selected = [] # List of plasmids to plot
 data_selected = data[(data.group.isin(selected)) & (data.iso == 'iso400')]
-data_selected.to_csv('/content/gdrive/Shareddrives/✨ NOUKA ✨/Ноучные эксперименты/nouka26. Paper about bioluminescent system 2.0/01. Preparing draft/Data/data15/exp948/exp948_data_selected.csv')
+data_selected.to_csv('') # Insert here path to save processed data
+
+mann_whitney_test = scihoc.posthoc_mannwhitney(data_selected, val_col = 'Mean', group_col = 'group', p_adjust = 'holm-sidak')
+
+fig, axes = plt.subplots(1, figsize = (18, 5))
+
+medianprops = {'color': 'coral'}
+capprops = {'color': 'white'}
+whiskerprops = {'color': 'white'}
+boxprops = {'edgecolor': 'white'}
+
+sorting_order = data_selected.groupby('group').median().sort_values('Mean', ascending=True).index.values
+
+ax2 = axes[1]
+
+sns.boxplot(data=data_selected,
+            x='group',
+            y='Mean',
+            color = 'black',
+            order = sorting_order,
+            medianprops = medianprops,
+            capprops = capprops,
+            whiskerprops = whiskerprops,
+            boxprops = boxprops,
+            ax = ax2)
+sns.swarmplot(data=data_selected, x='group', y='Mean', hue = 'nLabel', order = sorting_order, ax = ax2)
+
+ax2.set_yscale('log')
+ax2.set_xlabel(None)
+ax2.set_ylabel(u'Luminescence, RLU', fontsize = ylabel_size)
+ax2.set_title(r'$\it{Nicotiana}$' + ' ' + r'$\it{tabacum}$' + '\n', fontsize = title_size, color = '#ffdfff')  # Insert here a title. Example is provided
+
+ax2.legend().set_visible(False)
+
+ax2.grid(lw = 0.3)
+ax2.set_ylim(0.7)
+ax2.set_xticks(ticks = np.arange(0, len(sorting_order), 1))
+ax2.set_xticklabels(labels = [FBP[x] for x in sorting_order])
+ax2.tick_params(axis = 'y', labelsize = yticklabel_size)
+ax2.tick_params(axis = 'x', labelsize = xticklabel_size)
+
+# Example of statistical annotation from Figure 2b
+# statistical annotation
+x1, x2 = 0, 1
+y, h, col = 2.3, 0.2, 'white'
+ax2.plot([x1, x1, x2, x2], [y, y-h, y-h, y], lw=1.5, c=col)
+p_value = f'{round(np.divide(data_selected[data_selected["group"] == "pNK497"]["Mean"].mean(), data_selected[data_selected["group"] == "pX037"]["Mean"].mean()) ,1)}-fold\n \
+{stat_rounder(mann_whitney_test["pNK497"]["pX037"])}'
+ax2.text((x1+x2)*.5, y-2.1*h, p_value, ha='center', va='top', fontsize = xticklabel_size)
+
+plt.subplots_adjust(hspace=4)
+sns.despine(offset = 10, trim = False)
+plt.tight_layout(w_pad = 2.9)
+
+plt.savefig('', # Insert here path for figure to save
+            dpi = 400,
+            bbox_inches='tight',
+            transparent=False,
+            facecolor='black')
